d6de48aa339222521e2c65234595178e5baa1f62
shenjie <1075850619@qq.com>
2018-07-13 12:12:16 +0800

    Initial commit

---
+++ python-syntax/com/blue/python/syntax/demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding:UTF-8
+
+if True:
+    print "true"    # 如果为真(需指定为带有中文的编码格式，例如coding:UTF-8)
+else:
+  print "false"
+  print "is false"
+
+print "answer ok"
+print("end")
