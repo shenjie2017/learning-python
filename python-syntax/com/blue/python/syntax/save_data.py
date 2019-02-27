01b4cd2f029bf4ae9c6e9b1bdf442552117be749
Jason <1075850619@qq.com>
2019-02-27 10:28:39 +0800

    编写了类继承、读写文件、存取数据和测试案例等序列化框架

--- python-syntax/com/blue/python/syntax/save_data.py
+++ python-syntax/com/blue/python/syntax/save_data.py
@@ -2,6 +2,9 @@ import json
 
 names = ["zhangsan", "lisi", "wangwu", 666]
 zhangsan_info = {"name": "zhangsan", "age": 18, "sex": "man"}
+lisi_info = {"name": "lisi", "age": 24, "sex": "woman"}
+
+peoples = [zhangsan_info,lisi_info]
 
 with open("test03.json", "w") as file:
     json.dump(zhangsan_info, file)
@@ -14,5 +17,12 @@ with open("test04.json", "w") as file:
     json.dump(names, file)
 
 with open("test04.json", "r") as file:
+    arr = json.load(file)
+    print(arr)
+
+with open("test05.json", "w") as file:
+    json.dump(peoples, file)
+
+with open("test05.json", "r") as file:
     arr = json.load(file)
     print(arr)
